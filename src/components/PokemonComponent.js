1c6cd4eca24962987b8a54709a829403dc3fb5de
peppone-choi <peppone.choi@gmail.com>
2024-05-13 19:58:01 +0900

    포켓몬 컴포넌트 수정

--- src/components/PokemonComponent.js
+++ src/components/PokemonComponent.js
@@ -6,7 +6,18 @@ import { bindPokemonData, bindPokemonSpecies } from '../api/pokemonService';
 
 const BackgroundDiv = styled.div`
   background: ${(props) => props.color};
-  ${tw`block text-center text-white shadow-lg rounded-2xl w-60 h-72`}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  ${tw`text-white shadow rounded-2xl w-60 h-72`}
+`;
+
+const PokemonNameDiv = styled.div`
+  color: ${(props) => props.color};
+  background: white;
+  text-align: center;
+  ${tw`items-center justify-center w-48 p-1 mb-1 text-2xl font-bold shadow rounded-2xl`}
 `;
 
 const PokemonComponent = ({ urls }) => {
@@ -32,10 +43,10 @@ const PokemonComponent = ({ urls }) => {
       color={`#${type !== 'stellar' ? typeObject.hex.dark : `conic-gradient(#ceb344 8%, #d09949 8% 15%, #ce684c 15% 19%, #c9606e 19% 22%, #b975a9 22% 25%, #9f5b89 25% 28%, #694d94 28% 31%, #63537a 31% 35%, #596677 35% 42%, #5f708d 42% 50%, #4887be 50% 58%, #3d9cc4 58% 65%, #428dc4 65% 69%, #457cbb 69% 72%, #4095b9 72% 75%, #61b7c2 75% 78%, #56c267 78% 81%, #a5b65b 81% 85%, #aaaf80 85% 92%, #b69867 0);`}`}
       data-testid={`pokemon-card-${pokemonId}`}
     >
-      <div className="flex justify-end mx-2 mt-2 text-sm">
-        <p>#{String(pokemonId).padStart(3, '0')}</p>
+      <div className="flex justify-end w-full text-sm">
+        <p className="mr-4">#{String(pokemonId).padStart(3, '0')}</p>
       </div>
-      <div className="flex justify-center mb-4">
+      <div className="flex justify-center mb-1">
         <img
           role="figure"
           className="size-44"
@@ -44,9 +55,12 @@ const PokemonComponent = ({ urls }) => {
           alt="포켓몬 이미지"
         />
       </div>
-      <div aria-labelledby="heading">
+      <PokemonNameDiv
+        color={`#${type !== 'stellar' ? typeObject.hex.dark : `conic-gradient(#ceb344 8%, #d09949 8% 15%, #ce684c 15% 19%, #c9606e 19% 22%, #b975a9 22% 25%, #9f5b89 25% 28%, #694d94 28% 31%, #63537a 31% 35%, #596677 35% 42%, #5f708d 42% 50%, #4887be 50% 58%, #3d9cc4 58% 65%, #428dc4 65% 69%, #457cbb 69% 72%, #4095b9 72% 75%, #61b7c2 75% 78%, #56c267 78% 81%, #a5b65b 81% 85%, #aaaf80 85% 92%, #b69867 0);}`}`}
+        aria-labelledby="heading"
+      >
         <h1>{pokemonSpecies.koreanName}</h1>
-      </div>
+      </PokemonNameDiv>
       <div>
         <h2>{typeObject.name} 포켓몬</h2>
       </div>
