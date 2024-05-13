7cc419dc6ee5dc0df48805cc0a23317267d2a12e
peppone-choi <peppone.choi@gmail.com>
2024-05-13 23:48:53 +0900

    버튼 제작 완료

--- src/components/PokemonComponent.js
+++ src/components/PokemonComponent.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import tw, { styled } from 'twin.macro';
 import { pokemonInstance } from '../api/axios';
 import { bindPokemonData, bindPokemonSpecies } from '../api/pokemonService';
@@ -26,6 +27,7 @@ const PokemonComponent = ({ urls, typeData }) => {
   const [pokemonSpecies, setPokemonSpecies] = React.useState({});
   const type = pokemonData.types?.[0].type.name;
   const typeInfo = typeData.find((item) => item.type === type);
+  const navigatre = useNavigate();
   useEffect(() => {
     async function fetchPokemonData() {
       const boundPokemonSpecie = bindPokemonSpecies((await pokemonInstance.get(`${speciesUrl}`)).data);
@@ -41,25 +43,32 @@ const PokemonComponent = ({ urls, typeData }) => {
       color={`#${type !== 'stellar' ? typeInfo?.hex.normal : `conic-gradient(#ceb344 8%, #d09949 8% 15%, #ce684c 15% 19%, #c9606e 19% 22%, #b975a9 22% 25%, #9f5b89 25% 28%, #694d94 28% 31%, #63537a 31% 35%, #596677 35% 42%, #5f708d 42% 50%, #4887be 50% 58%, #3d9cc4 58% 65%, #428dc4 65% 69%, #457cbb 69% 72%, #4095b9 72% 75%, #61b7c2 75% 78%, #56c267 78% 81%, #a5b65b 81% 85%, #aaaf80 85% 92%, #b69867 0);`}`}
       data-testid={`pokemon-card-${pokemonId}`}
     >
-      <div className="flex justify-end w-full text-sm">
-        <p className="mr-4">#{String(pokemonId).padStart(3, '0')}</p>
-      </div>
-      <div className="flex justify-center mb-1">
-        <img
-          role="figure"
-          className="size-44"
-          key={`pokemon-${pokemonId}-${pokemonSpecies.koreanName}`}
-          src={pokemonData.image}
-          alt="포켓몬 이미지"
-        />
-      </div>
-      <PokemonNameDiv
-        color={`#${type !== 'stellar' ? typeInfo?.hex.dark : `conic-gradient(#ceb344 8%, #d09949 8% 15%, #ce684c 15% 19%, #c9606e 19% 22%, #b975a9 22% 25%, #9f5b89 25% 28%, #694d94 28% 31%, #63537a 31% 35%, #596677 35% 42%, #5f708d 42% 50%, #4887be 50% 58%, #3d9cc4 58% 65%, #428dc4 65% 69%, #457cbb 69% 72%, #4095b9 72% 75%, #61b7c2 75% 78%, #56c267 78% 81%, #a5b65b 81% 85%, #aaaf80 85% 92%, #b69867 0);}`}`}
-        aria-labelledby="heading"
+      <button
+        type="button"
+        onClick={() => {
+          navigatre(`/${pokemonId}`);
+        }}
       >
-        <h1>{pokemonSpecies.koreanName}</h1>
-        <h2 className="text-sm">{pokemonSpecies.name}</h2>
-      </PokemonNameDiv>
+        <div className="flex justify-end w-full text-sm">
+          <p className="mr-4">#{String(pokemonId).padStart(3, '0')}</p>
+        </div>
+        <div className="flex justify-center mb-1">
+          <img
+            role="figure"
+            className="size-44"
+            key={`pokemon-${pokemonId}-${pokemonSpecies.koreanName}`}
+            src={pokemonData.image}
+            alt="포켓몬 이미지"
+          />
+        </div>
+        <PokemonNameDiv
+          color={`#${type !== 'stellar' ? typeInfo?.hex.dark : `conic-gradient(#ceb344 8%, #d09949 8% 15%, #ce684c 15% 19%, #c9606e 19% 22%, #b975a9 22% 25%, #9f5b89 25% 28%, #694d94 28% 31%, #63537a 31% 35%, #596677 35% 42%, #5f708d 42% 50%, #4887be 50% 58%, #3d9cc4 58% 65%, #428dc4 65% 69%, #457cbb 69% 72%, #4095b9 72% 75%, #61b7c2 75% 78%, #56c267 78% 81%, #a5b65b 81% 85%, #aaaf80 85% 92%, #b69867 0);}`}`}
+          aria-labelledby="heading"
+        >
+          <h1>{pokemonSpecies.koreanName}</h1>
+          <h2 className="text-sm">{pokemonSpecies.name}</h2>
+        </PokemonNameDiv>
+      </button>
     </BackgroundDiv>
   );
 };
