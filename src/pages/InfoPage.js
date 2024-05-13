4a422d52031c2944136a2fe67c0992de34f4b8e5
peppone-choi <peppone.choi@gmail.com>
2024-05-14 00:32:03 +0900

    제출 예정

--- src/pages/InfoPage.js
+++ src/pages/InfoPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import tw, { styled } from 'twin.macro';
 import { getPokemonDataById, getPokemonSpeciesById, bindPokemonData, bindPokemonSpecies } from '../api/pokemonService';
 
@@ -23,6 +23,7 @@ const InfoPage = ({ typeData }) => {
   const { id } = useParams();
   const [pokemonData, setPokemonData] = useState([]);
   const [speciesData, setSpeciesData] = useState({});
+  const navigate = useNavigate();
   const type = pokemonData.types?.[0].type.name;
   const typeInfo = typeData.pokemonTypes.find((item) => item.type === type);
   useEffect(() => {
@@ -34,8 +35,16 @@ const InfoPage = ({ typeData }) => {
     }
     petchPokemonData();
   }, [id]);
+
+  const foward = () => {
+    navigate(`/${Number(id) + 1}`);
+  };
+
+  const back = () => {
+    navigate(`/${Number(id) - 1}`);
+  };
   return (
-    <div className="flex justify-center">
+    <div className="flex items-center justify-center mt-10">
       <BackgroundDiv
         color={`#${type !== 'stellar' ? typeInfo?.hex.normal : `conic-gradient(#ceb344 8%, #d09949 8% 15%, #ce684c 15% 19%, #c9606e 19% 22%, #b975a9 22% 25%, #9f5b89 25% 28%, #694d94 28% 31%, #63537a 31% 35%, #596677 35% 42%, #5f708d 42% 50%, #4887be 50% 58%, #3d9cc4 58% 65%, #428dc4 65% 69%, #457cbb 69% 72%, #4095b9 72% 75%, #61b7c2 75% 78%, #56c267 78% 81%, #a5b65b 81% 85%, #aaaf80 85% 92%, #b69867 0);`}`}
         data-testid={`pokemon-card-${id}`}
@@ -58,9 +67,9 @@ const InfoPage = ({ typeData }) => {
         </PokemonNameDiv>
         <div>
           <div className="flex items-center justify-between">
-            <p>{pokemonData.height}m</p>
+            <p className="ml-10">{pokemonData.height}m</p>
             <p>{pokemonData.weight}kg</p>
-            <div>
+            <div className="mr-10">
               {pokemonData.abilityKoreanNames?.map((ability) => (
                 <div key={ability} className="text-center">
                   {ability}
@@ -74,8 +83,21 @@ const InfoPage = ({ typeData }) => {
               <div key={stat.name} className="font-semibold text-center">{`${stat.name}: ${stat.value}`}</div>
             ))}
           </div>
-          <div className="m-5">
+          <div className="m-5 text-center w-96">
             <p>{speciesData.koreanFlavorText}</p>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center justify-between w-96 ">
+            <button type="button" className="mr-24 text-4xl size-11" onClick={back}>
+              <p>{'<'}</p>
+            </button>
+
+            <p>{speciesData.id}</p>
+            <p>{speciesData.name}</p>
+            <button type="button" className="ml-24 text-4xl size-11" onClick={foward}>
+              <p>{'>'}</p>
+            </button>
           </div>
         </div>
       </BackgroundDiv>
